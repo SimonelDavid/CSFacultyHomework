53ddd752c1b35f862dc176340bcf7a6e376e3b36
Simonel David <simoneldavid@Simonels-MacBook-Pro.local>
2022-10-17 15:22:36 +0300

    added prep for homework

---
+++ Third_semester/MAP/homework_1/seminar/src/main/java/org/example/seminar1/Main.java
@@ -0,0 +1,84 @@
+package org.example.seminar1;
+
+import org.example.seminar1.factories.Strategy;
+import org.example.seminar1.models.Message;
+import org.example.seminar1.models.MessageTask;
+import org.example.seminar1.models.Task;
+import org.example.seminar1.runners.PrinterTaskRunner;
+import org.example.seminar1.runners.StrategyTaskRunner;
+import org.example.seminar1.runners.TaskRunner;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //testMessage();
+        testTaskRunner();
+
+    }
+
+    private static void testMessage() {
+        Message message1 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task1 = new MessageTask("1", "d1", message1);
+
+        Message message2 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task2 = new MessageTask("2", "d2", message2);
+
+        Message message3 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task3 = new MessageTask("3", "d3", message3);
+
+        Message message4 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task4 = new MessageTask("4", "d4", message4);
+
+        Message message5 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task5 = new MessageTask("5", "d5", message5);
+
+        List<Task> tasks = new LinkedList<>();
+        tasks.add(task1);
+        tasks.add(task2);
+        tasks.add(task3);
+        tasks.add(task4);
+        tasks.add(task5);
+
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
+    }
+    
+    private static void testTaskRunner() {
+        Message message1 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task1 = new MessageTask("1", "d1", message1);
+
+        Message message2 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task2 = new MessageTask("2", "d2", message2);
+
+        Message message3 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task3 = new MessageTask("3", "d3", message3);
+
+        Message message4 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task4 = new MessageTask("4", "d4", message4);
+
+        Message message5 = new Message("hello", "mircea", "fenesan", LocalDateTime.now());
+        Task task5 = new MessageTask("5", "d5", message5);
+
+        List<Task> tasks = new LinkedList<>();
+        tasks.add(task1);
+        tasks.add(task2);
+        tasks.add(task3);
+        tasks.add(task4);
+        tasks.add(task5);
+
+        TaskRunner strategyTaskRunner = new StrategyTaskRunner(Strategy.LIFO);
+        TaskRunner printerTaskRunner = new PrinterTaskRunner(strategyTaskRunner);
+
+        for (Task task : tasks) {
+//            strategyTaskRunner.addTask(task);
+            printerTaskRunner.addTask(task);
+        }
+
+//        strategyTaskRunner.executeAll();
+        printerTaskRunner.executeAll();
+    }
+}
---
+++ Third_semester/MAP/homework_1/seminar/src/main/java/org/example/seminar1/runners/TaskRunner.java
@@ -0,0 +1,13 @@
+package org.example.seminar1.runners;
+
+import org.example.seminar1.models.Task;
+
+public interface TaskRunner {
+    void executeOneTask();//executa un task din colecţia de task-uri de executat
+
+    void executeAll();//executǎ toate task-urile din colecţia de task-uri.
+
+    void addTask(Task t);//adaugǎ un task în colecţia de task-uri de executat
+
+    boolean hasTask();//verifica daca mai sunt task-ri de executat
+}
